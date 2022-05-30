854490cbb518a9e879e380b33a5352de61a5c0b1
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2022-05-30 15:07:28 +0200

    Fullscren + fix gallery header

--- front/JS/gallery.js
+++ front/JS/gallery.js
@@ -1,98 +1,115 @@
-// To prevent the user from accessing the data directly
-(function () {
-    var request = new XMLHttpRequest();
-    request.open("get", "back/loadGallery.php", true);
-    request.send();
-    request.onload = displayGallery;
+var request = new XMLHttpRequest();
+request.open("get", "back/loadGallery.php", true);
+request.send();
+request.onload = displayGallery;
 
-    function displayGallery() {
-        allFiles = JSON.parse(this.responseText);
-        console.log(allFiles);
-        allFiles.forEach((file) => {
-            displayFile(file);
-        });
-    }
+function displayGallery() {
+    allFiles = JSON.parse(this.responseText);
+    console.log(allFiles);
+    allFiles.forEach((file) => {
+        displayFile(file);
+    });
+}
 
-    function displayFile(file) {
-        // TMP
-        file.Tags = ["tag1", "tag2"];
+function displayFile(file) {
+    // TMP
+    file.Tags = ["tag1", "tag2"];
 
-        let path = "./upload/" + file.IDFichier + "." + file.Extension;
-        console.log(path);
-        //path = "./upload/test.png";
+    let path = "./upload/" + file.IDFichier + "." + file.Extension;
 
-        let container = $("<div>").addClass("file-container");
-        let preview;
-        if (file.Type == "image") {
-            preview = $("<img />").attr("src", path);
-        } else {
-            preview = $("<video />");
-            let source = $("<source />")
-                .attr("src", path)
-                .attr("type", file.Type + "/" + file.Extension);
-            preview.append(source);
+    let container = $("<div>").addClass("file-container");
+    let preview;
+    if (file.Type == "image") {
+        preview = $("<img />").attr("src", path);
+    } else {
+        preview = $("<video />");
+        let source = $("<source />")
+            .attr("src", path)
+            .attr("type", file.Type + "/" + file.Extension);
+        preview.append(source);
 
-            /* preview.hover(function toggleControls() {
+        /* preview.hover(function toggleControls() {
                 if (this.hasAttribute("controls")) {
                     this.removeAttribute("controls");
                 } else {
                     this.setAttribute("controls", "controls");
                 }
             }); */
-        }
-        preview.addClass("file-preview");
-        let hover = $("<div>").addClass("file-hover");
-        let hoverTags = $("<div>").addClass("file-hover-tags");
-        file.Tags.forEach((tag) => {
-            let tagElem = $("<div>")
-                .addClass("tag")
-                .html("<p>" + tag + "</p>");
-            hoverTags.append(tagElem);
-        });
-        let title = $("<div>").addClass("file-hover-title").text(file.NomFichier);
-        let author = $("<div>")
-            .addClass("file-hover-author")
-            .text("de " + file.Prenom + " " + file.Nom + ", " + file.Date);
-        let info = $("<div>")
-            .addClass("file-hover-info")
-            .text(bytesToSize(file.Taille) + (file.Duree != "0" ? " - " + formatSeconds(file.Duree) : ""));
-        hover.append(hoverTags);
-        hover.append(title);
-        hover.append(author);
-        hover.append(info);
-
-        container.append(preview);
-        container.append(hover);
-
-        $("#gallery").append(container);
     }
+    preview.addClass("file-preview");
+    container.dblclick(displayInFullscreen);
+    let hover = $("<div>").addClass("file-hover");
+    let hoverTags = $("<div>").addClass("file-hover-tags");
+    file.Tags.forEach((tag) => {
+        let tagElem = $("<div>")
+            .addClass("tag")
+            .html("<p>" + tag + "</p>");
+        hoverTags.append(tagElem);
+    });
+    let title = $("<div>").addClass("file-hover-title").text(file.NomFichier);
+    let author = $("<div>")
+        .addClass("file-hover-author")
+        .text("de " + file.Prenom + " " + file.Nom + ", " + file.Date);
+    let info = $("<div>")
+        .addClass("file-hover-info")
+        .text(bytesToSize(file.Taille) + (file.Duree != "0" ? " - " + formatSeconds(file.Duree) : ""));
+    hover.append(hoverTags);
+    hover.append(title);
+    hover.append(author);
+    hover.append(info);
 
-    function bytesToSize(bytes) {
-        if (bytes == 0) {
-            return "0 Octet";
-        }
-        let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024))),
-            sizes = ["Octets", "Ko", "Mo", "Go", "To"];
-        return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+    container.append(preview);
+    container.append(hover);
+
+    $("#gallery").append(container);
+}
+
+function bytesToSize(bytes) {
+    if (bytes == 0) {
+        return "0 Octet";
     }
+    let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024))),
+        sizes = ["Octets", "Ko", "Mo", "Go", "To"];
+    return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+}
 
-    // Convert seconds to MM:SS or HH:MM:SS
-    function formatSeconds(s) {
-        let sec_num = parseInt(s, 10);
-        let hours = Math.floor(sec_num / 3600);
-        let minutes = Math.floor((sec_num - hours * 3600) / 60);
-        let seconds = sec_num - hours * 3600 - minutes * 60;
+// Convert seconds to MM:SS or HH:MM:SS
+function formatSeconds(s) {
+    let sec_num = parseInt(s, 10);
+    let hours = Math.floor(sec_num / 3600);
+    let minutes = Math.floor((sec_num - hours * 3600) / 60);
+    let seconds = sec_num - hours * 3600 - minutes * 60;
 
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        if (hours > 0) {
-            if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
-            return hours + ":" + minutes + ":" + seconds;
-        } else {
-            return minutes + ":" + seconds;
-        }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
     }
-})();
+    if (hours > 0) {
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        return hours + ":" + minutes + ":" + seconds;
+    } else {
+        return minutes + ":" + seconds;
+    }
+}
+
+function displayInFullscreen(event) {
+    let preview = event.currentTarget.children[0].cloneNode(true);
+    // Select "#fullscreen-container" or create it if it doesn't exist
+    let container = $("#fullscreen-container");
+    if (container.length == 0) {
+        container = $("<div>").attr("id", "fullscreen-container");
+        $("body").append(container);
+    }
+    // If the priview is a video, add controls
+    if (preview.tagName == "VIDEO") {
+        preview.setAttribute("controls", "controls");
+        preview.style.backgroundColor = "black";
+    }
+    container.append(preview);
+    container.click(function(e) {
+        if (e.target !== this) return;
+        container.remove();
+    });
+    console.log(preview);
+}
