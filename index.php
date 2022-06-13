af6bea33b2c50e13aae7d9a05cfc77037d8452d0
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2022-06-13 14:10:38 +0200

    Fix scroll bar gallery

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($_SESSION['id']) {
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="front/css/index.css" />
-    <link rel="stylesheet" href="front/css/tag.css" />    
+    <link rel="stylesheet" href="front/css/tag.css" />
     <script src="https://unpkg.com/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="front/js/gallery.js" defer></script>
@@ -30,36 +30,37 @@ if ($_SESSION['id']) {
 
     <div id="content">
         <div id="barre" class="undraggable">
+            <div id="barre-scroll">
+                <h1 class="barre-title">Selection multiple</h1>
+                <div id="selection-multiple-toggle">Désactivé</div>
+                <div id="selection-multiple">
+                    <p><span id="selection-multiple-size">513 Go</span> de fichiers selectionnés</p>
+                    <select id="selection-multiple-select">
+                        <option value="" selected disabled>Tag</option>
+                    </select>
+                    <button>Ajouter</button>
+                    <button>Supprimer</button>
+                </div>
+                <h1 class="barre-title">Fonctions de tri</h1>
+                <div id="tri-primaire">
+                    <div class="pre-tri" id="toggle-mes-fichiers">
+                        <p>Mes fichiers</p>
+                    </div>
+                    <div class="pre-tri Corbeille" id="toggle-corbeille">
+                        <p>Corbeille</p>
+                    </div>
+                    <div class="pre-tri tout-type" id="toggle-type-fichier">
+                        <p>Image / Vidéo</p>
+                    </div>
+                    <div class="pre-tri Intersection" id="toggle-type-tri-tag">
+                        <p>Intersection</p>
+                    </div>
+                </div>
 
-            <h1 class="barre-title">Selection multiple</h1>
-            <div id="selection-multiple-toggle">Désactivé</div>
-            <div id="selection-multiple">
-                <p><span id="selection-multiple-size">513 Go</span> de fichiers selectionnés</p>
-                <select id="selection-multiple-select">
-                    <option value="" selected disabled>Tag</option>
-                </select>
-                <button>Ajouter</button>
-                <button>Supprimer</button>
-            </div>
-            <h1 class="barre-title">Fonctions de tri</h1>
-            <div id="tri-primaire">
-                <div class="pre-tri" id="toggle-mes-fichiers">
-                    <p>Mes fichiers</p>
-                </div>
-                <div class="pre-tri Corbeille" id="toggle-corbeille">
-                    <p>Corbeille</p>
-                </div>
-                <div class="pre-tri tout-type" id="toggle-type-fichier">
-                    <p>Image / Vidéo</p>
-                </div>
-                <div class="pre-tri Intersection" id="toggle-type-tri-tag">
-                    <p>Intersection</p>
-                </div>
-            </div>
-
-            <div id="liste-categories">
-                <!-- Ici s'appenderont les categories -->
                 <h1 class="barre-title">Trier par tags</h1>
+                <div id="liste-categories">
+                    <!-- Ici s'appenderont les categories -->
+                </div>
             </div>
 
             <?php if ($_SESSION['role'] != "lecture") { ?>
