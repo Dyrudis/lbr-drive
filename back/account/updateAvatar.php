52014aba666c334532594e642894778ee5483b30
Disleif <58111527+Disleif@users.noreply.github.com>
2022-06-20 14:05:10 +0200

    add log on avatar change

--- back/account/updateAvatar.php
+++ back/account/updateAvatar.php
@@ -18,6 +18,11 @@ if ($tmpAvatarPath == "") {
 $newAvatarPath = "../../avatars/" . $IDUtilisateur;
 
 if (move_uploaded_file($tmpAvatarPath, $newAvatarPath)) {
+
+    // INSERT LOG
+    include '../log/registerLog.php';
+    registerNewLog($mysqli, $IDUtilisateur, "Modifie son avatar");
+
     echo "OK";
 } else {
     echo "Erreur lors de la modification de la photo de profile.";
